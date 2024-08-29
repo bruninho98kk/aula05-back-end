cf76ea909e588717d1648894d566026019d4f41c
bruno <brunoquintino.tds2.senai@gmail.com>
2024-08-29 11:49:17 -0300

    feat: start to refactor api routes

---
+++ src/routes/emocoes.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express"
+
+const emocoesRoutes = Router()
+
+const emocoes = [
+    {
+    app.get("/emocoes",(req, res) => {
+        return res.status(200)
+        .send( emocoes )
+        }) 
+    }
+]
