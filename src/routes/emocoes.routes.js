a96cc5e78f563d9fa3ec4d07e7557cea5b1e2499
bruno <brunoquintino.tds2.senai@gmail.com>
2024-09-26 15:16:43 -0300

    feat: create delete emotion method

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -1,6 +1,6 @@
-import { Router } from "express"
+import { Router } from "express";
 
-const emocoesRoutes = Router()
+const emocoesRoutes = Router();
 
 let emocoes = [
     {
@@ -60,5 +60,40 @@ return res.status(200).send({
 
 });
 
+emocoesRoutes.put("/:id", (req, res) => {
+    const{ id } = req.params;
 
+    const emocao = emocoes.find((emotion) => emotion.id = id);
+    if(!emocao) {
+        return res.status(404).send({
+            message: "Emoção não encontra!"
+        })
+    }
+    const {nome, cor} = req.body
+    emocao.nome = nome;
+    emocao.cor = cor;
+
+    return res.status(200).send({
+        message: "Emoção atualizada!",
+        emocao,
+    })
+});
+
+emocoesRoutes.delete("/:id", (req, res) => {
+    const{ id } = req.params;
+
+    const emocao = emocoes.find((emotion) => emotion.id = id);
+    if(!emocao) {
+        return res.status(404).send({
+            message: "Emoção não encontrada!"
+        })
+    }
+
+    emocoes = emocoes.filter((emotion) => emotion.id != id);
+    
+    return res.status(200).send({
+        message: "Emoção deletada!",
+        emocao,
+    });
+});
 export default emocoesRoutes;
